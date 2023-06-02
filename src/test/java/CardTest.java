f997bd3a13405560da18b92897ca352569598c6d
Кочнев Илья <Bankir369@gmail.com>
2023-06-02 23:53:11 +0300

    Inicial commit

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -9,8 +9,8 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    String generateDate(int daysToAdd, String pattern) {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern(pattern));
 
     }
 
@@ -19,12 +19,12 @@ public class CardTest {
         open("http://localhost:9999/");
         $("[placeholder='Город']").setValue("Казань");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.yyyy"));
         $("[name='name']").setValue("Иванов Петр");
         $("[name='phone']").setValue("+79033237979");
         $("[data-test-id='agreement']").click();
         $$("[type='button']").find(Condition.exactText("Забронировать")).click();
-        String planningDate = generateDate(4);
+        String planningDate = generateDate(3, "dd.MM.yyyy");
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
